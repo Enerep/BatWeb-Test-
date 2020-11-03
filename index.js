f7d2f7adaecb86c1b743dd1eb8e1aca93a503b80
Bat-Ochir <47132106+Enerep@users.noreply.github.com>
2020-11-03 20:26:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function ScrollFunc() {
-	var element = document.querySelector(".goto");
+	var element = document.querySelector(".InfoP");
 
 	element.scrollIntoView({ behavior: 'smooth', block: 'end'});
 }
